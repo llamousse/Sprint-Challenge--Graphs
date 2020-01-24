dc7f10498876580a6d59abc13b0786efe74c153e
Vicky <vyue013@gmail.com>
2020-01-24 11:17:12 -0800

    mvp

--- adv.py
+++ adv.py
@@ -13,7 +13,7 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+# # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
@@ -28,9 +28,51 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = ['n', 'e', 's', 'w']
 
+def traversePath(graph):
+    # initialize empty maze and rooms array
+    maze = []
+    rooms = []
 
+    # make a set to keep track of where we've been
+    visited = set()
+
+    # set maze starting point 0
+    maze.append(0)
+
+    # start until all rooms have been visited
+    # while length of visited is less than length of map graph
+    while len(visited) < len(graph):
+        # pop the first item and set in current_room
+        current_room = maze[-1]
+        # add to set of visited rooms
+        visited.add(current_room)
+        # add to connections & declare free connects
+        connect = graph[current_room][1]
+        free_connections = []
+
+        for curr_room, unchecked_room in connect.items():
+            # if unchecked room is not in visited list
+            if unchecked_room not in visited:
+                # do thing and add in free_connections
+                free_connections.append(unchecked_room)
+        
+        # if there are freee connections, add them to room and maze
+        if len(free_connections) > 0:
+            room = free_connections[0]
+            maze.append(free_connections[0])
+        else:
+            room = maze[-2]
+            maze.pop()
+        
+        # if no where else to go, go back to previous room
+        for previous_room, exit_door in connect.items():
+            if exit_door == room:
+                rooms.append(previous_room)
+        
+    return rooms
+
+traversal_path = traversePath(room_graph)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -46,18 +88,18 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
+print(f"MOVES MADE: {len(traversal_path)}")
 
 
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
